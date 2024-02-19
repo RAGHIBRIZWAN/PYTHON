d0cb63c0eeac5e79194f79b80bdf5bfa5a9937fc
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-02-19 15:06:19 +0500

    Create file1.py

---
+++ Syntax/file1.py
@@ -0,0 +1,22 @@
+print(3 > 2)
+print(3 < 2)
+print(3 >= 2)
+print(3 <= 2)
+print(3 == 2)
+print(2 == 2)
+print(3 != 2)
+print(3 != 3)
+
+
+#logical operators
+
+#atleast one is true then print true (or)
+print(2 > 3 or 2 > 1)
+
+# if both are true then print true (and)
+print(2 > 3 and 2 > 1)
+print(4 > 3 and 2 > 1)
+
+#not operator : converts false into true and vice versa
+
+print(not 2 > 3)
