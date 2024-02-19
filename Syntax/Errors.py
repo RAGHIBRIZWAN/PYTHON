d6022f42c04915d1a23ad8e662b2097e0b6089f0
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-02-19 15:20:52 +0500

    Create Errors.py

---
+++ Syntax/Errors.py
@@ -0,0 +1,10 @@
+# ERRORS THAT CAN HANDLE
+
+# 1) Zero Division: 1/0
+# 2) Name Error: print(b) but b is not defined
+# 3) Type Error: use str + num then error will be occured
+# 4) Value Error: int(input("enter value")) --- print("Hello")
+# 5) Index Error: l=[10,20,30,40] --- print(l[6]).....There is no 6 Index
+# 6) Key Error: d = {"name":"Raghib"} --- print(d[f_name])....There is no f_name key
+# 7) Module Not Found Error: cal.py..... import cals
+# 8) Import Error: cal.py....def sum():...print(10+20)....from cal import sum1....There is no sum1
